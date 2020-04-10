566aa565acfb3d43ce2f033acb5d3a637c79571f
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2020-04-10 18:13:50 -0500

    Se ha agregado mas contenido, y se han ajustado los estilos

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,24 @@
 // funsion para cambiar el tema del proyecto
+var style = localStorage.getItem('style') || 'verde';
 
 function changeColor(color){
-	var style = $('#style').attr('href', `styles/${color}.css`);
-	console.log(style);
+	const style = `styles/${color}.css`;
+	$('#style').attr('href', style);
+	localStorage.setItem('style', color);	
+}
+
+changeColor(style);	
+
+//Funsion para hacer submit del formulario
+function onSubmit(event){
+	const { name, email, message } = event.target
+	event.preventDefault();
+	alert(`Gracias ${name.value} por contactar con nosotros, ahora es nuestro turno. Tendras nuestra respuesta a tu pregunta en el correo ${email.value}`);
+	$('#messages').append(`
+		<h1 class="name">${name.value}<h1>
+		<span><small class="date">${Date()}<small></span>
+		<p class="message">Mensage: ${message.value}</p>
+		<hr>
+	`);
+	document.getElementById('form').reset();
 }
